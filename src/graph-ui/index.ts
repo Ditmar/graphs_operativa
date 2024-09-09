3ab8c407a3d7fc284d2247e51c9e885349db56ff
Ditmar <ditmarcastro@gmail.com>
2024-09-09 02:38:43 -0400

    feat: powerfull graphs

--- src/graph-ui/index.ts
+++ src/graph-ui/index.ts
@@ -1,45 +1,68 @@
 import json from './city.json';
 import getCanvas from './canvas/canvas';
-const totalScale = 5000;
+import Vertex from '../graph/Vertex';
+import { calculateDistance, drawCircle, getVertex } from './utils';
+const totalScale = 4000;
+const positionX = 500;
+const positionY = 450;
+let integerX: any = null;
+let integerY: any = null;
 const scaleX = (lon: number) => (lon + 180) * (getCanvas().width / 360) * totalScale;
 const scaleY = (lat: number) => (90 - lat) * (getCanvas().height / 180) * totalScale;
 
+function transFormPoint(firstCoord: number, secondCoord: number) {
+    const coorsX = scaleX(firstCoord);
+    const coorsY = scaleY(secondCoord);
+    if (integerX == null && integerY == null) {
+        integerX = Math.floor(coorsX);
+        integerY = Math.floor(coorsY);
+    }
+    const x = coorsX - integerX + positionX;
+    const y = coorsY - integerY + positionY;
+    return {x , y};
+}
+
 export function drawGraph() {
     const canvas = getCanvas();
-    let integerX: any = null;
-    let integerY: any = null;
     const ctx = canvas.getContext('2d');
     if (ctx === null) {
         throw new Error('Failed to get 2D context');
     }
     ctx.strokeStyle = 'blue';
-    ctx.lineWidth = 0.5;
+    ctx.lineWidth = 0.3;
     ctx.fillStyle = 'black';
+    const graph: Record<string, Vertex> = {};
     json.features.forEach((feature: any) => {
-        
         if (feature.geometry.type === 'LineString') {
-            console.log(feature.properties);
             ctx.beginPath();
             feature.geometry.coordinates.forEach((line: number[], index: number) => {
                 const [firstCoord, secondCoord] = line;
-                const coorsX = scaleX(firstCoord);
-                const coorsY = scaleY(secondCoord);
-                if (integerX == null && integerY == null) {
-
-                    integerX = Math.floor(coorsX);
-                    integerY = Math.floor(coorsY);
-                }
-                const x = coorsX - integerX + 300;
-                const y = coorsY - integerY + 350;
+                const {x, y} = transFormPoint(firstCoord, secondCoord);
                 if (index === 0) {
                     ctx.moveTo(x, y);
                 } else {
-
+                    
                     ctx.lineTo(x, y);
                 }
-
+                drawCircle(x, y, 0.6);
+                const vertex = getVertex(graph, x, y);
+                if (index + 1 < feature.geometry.coordinates.length) {
+                    const next = feature.geometry.coordinates[index + 1];
+                    const [firstCoord, secondCoord] = next;
+                    const {x: x2, y: y2} = transFormPoint(firstCoord, secondCoord);
+                    const nextVertex = getVertex(graph, x2, y2);
+                    vertex?.addNeighbor(nextVertex, calculateDistance({x, y}, {x: x2, y: y2}));
+                }
+                if (index > 0) {
+                    const prev = feature.geometry.coordinates[index - 1];
+                    const [firstCoord, secondCoord] = prev;
+                    const {x: x2, y: y2} = transFormPoint(firstCoord, secondCoord);
+                    const prevVertex = getVertex(graph, x2, y2);
+                    vertex?.addNeighbor(prevVertex, calculateDistance({x, y}, {x: x2, y: y2}));
+                }
             });
-            ctx.stroke(); 
+            ctx.stroke();
         }
     });
+    return graph;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { drawGraph } from './graph-ui/index';
-drawGraph();
+import { Bfs } from './graph/paths/Bfs'
+const graph = drawGraph();
+
+console.log(graph['500.52501333341934_450.13266666652635']);
+Bfs(graph['500.52501333341934_450.13266666652635']);
 // import { createGraph } from './graph/main';
 
 // createGraph();
