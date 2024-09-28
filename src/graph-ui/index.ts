d3229fadb877fad27592e6fc33638b31392a18a5
Caleb  Coronado <coronadorodrigo668@gmail.com>
2024-09-27 21:16:15 -0400

    Lab Finalizado.

--- src/graph-ui/index.ts
+++ src/graph-ui/index.ts
@@ -7,6 +7,8 @@ const positionX = 500;
 const positionY = 450;
 let integerX: any = null;
 let integerY: any = null;
+
+// Escalado de coordenadas para ajustarse al canvas
 const scaleX = (lon: number) => (lon + 180) * (getCanvas().width / 360) * totalScale;
 const scaleY = (lat: number) => (90 - lat) * (getCanvas().height / 180) * totalScale;
 
@@ -19,7 +21,7 @@ function transFormPoint(firstCoord: number, secondCoord: number) {
     }
     const x = coorsX - integerX + positionX;
     const y = coorsY - integerY + positionY;
-    return {x , y};
+    return { x, y };
 }
 
 export function drawGraph() {
@@ -32,33 +34,33 @@ export function drawGraph() {
     ctx.lineWidth = 0.3;
     ctx.fillStyle = 'black';
     const graph: Record<string, Vertex> = {};
+
     json.features.forEach((feature: any) => {
         if (feature.geometry.type === 'LineString') {
             ctx.beginPath();
             feature.geometry.coordinates.forEach((line: number[], index: number) => {
                 const [firstCoord, secondCoord] = line;
-                const {x, y} = transFormPoint(firstCoord, secondCoord);
+                const { x, y } = transFormPoint(firstCoord, secondCoord);
                 if (index === 0) {
                     ctx.moveTo(x, y);
                 } else {
-                    
                     ctx.lineTo(x, y);
                 }
                 drawCircle(x, y, 0.6);
                 const vertex = getVertex(graph, x, y);
+
+                // Añadir conexiones entre los nodos
                 if (index + 1 < feature.geometry.coordinates.length) {
                     const next = feature.geometry.coordinates[index + 1];
-                    const [firstCoord, secondCoord] = next;
-                    const {x: x2, y: y2} = transFormPoint(firstCoord, secondCoord);
+                    const { x: x2, y: y2 } = transFormPoint(next[0], next[1]);
                     const nextVertex = getVertex(graph, x2, y2);
-                    vertex?.addNeighbor(nextVertex, calculateDistance({x, y}, {x: x2, y: y2}));
+                    vertex?.addNeighbor(nextVertex, calculateDistance({ x, y }, { x: x2, y: y2 }));
                 }
                 if (index > 0) {
                     const prev = feature.geometry.coordinates[index - 1];
-                    const [firstCoord, secondCoord] = prev;
-                    const {x: x2, y: y2} = transFormPoint(firstCoord, secondCoord);
+                    const { x: x2, y: y2 } = transFormPoint(prev[0], prev[1]);
                     const prevVertex = getVertex(graph, x2, y2);
-                    vertex?.addNeighbor(prevVertex, calculateDistance({x, y}, {x: x2, y: y2}));
+                    vertex?.addNeighbor(prevVertex, calculateDistance({ x, y }, { x: x2, y: y2 }));
                 }
             });
             ctx.stroke();
--- src/graph/paths/Dfs.ts
+++ src/graph/paths/Dfs.ts
@@ -2,7 +2,7 @@ import Vertex from "../Vertex";
 import getCanvas from '../../graph-ui/canvas/canvas';
 import { delay } from '../../graph-ui/utils';
 const ctx = getCanvas().getContext('2d');
-export const Dfs = (vertex: Vertex) => {
+export const Dfs = (vertex: Vertex) => { 
     vertex.getNeighbors().forEach(async(edge) => {
         const neighbor = edge.destination;
         if (neighbor && !neighbor.visited) {
