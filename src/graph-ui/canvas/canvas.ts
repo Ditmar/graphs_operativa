b8d8bd0d13ace99e0b9379251e88e95f009a2e47
AlvaroC <albarocondori0123@gmail.com>
2024-09-27 21:40:26 -0400

    Laboratorio 1

--- src/graph-ui/canvas/canvas.ts
+++
@@ -1,4 +0,0 @@
-function getCanvas() {
-    return document.getElementById('app') as HTMLCanvasElement;
-}
-export default getCanvas;
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { drawGraph } from './graph-ui/index';
-import { Bfs } from './graph/paths/Bfs'
-import { Dfs } from './graph/paths/Dfs'
-const graph = drawGraph();
-
-console.log(graph['500.52501333341934_450.13266666652635']);
-//Bfs(graph['500.52501333341934_450.13266666652635']);
-Dfs(graph['500.52501333341934_450.13266666652635']);
